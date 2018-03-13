a7418ad29e13119b805cb0b6bada18f1f945068a
Evan Thompson <evanbrowningthompson@gmail.com>
2018-03-13 12:49:17 -0400

    installed necessary enzyme react 16 adapter and fixed syntax error in first test file

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import 'Game' from './game';
+import Game from './game';
 
 describe('<Game />', () => {
   it('Renders without crashing', () => {
